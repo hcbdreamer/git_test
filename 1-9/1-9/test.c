66a7d740e38a0276f159c3319a9ddad01a424944
hcb_dreamer <38092117+hcbdreamer@users.noreply.github.com>
2018-11-07 15:58:15 +0800

    .gitignore is workoing

---
+++ 1-9/1-9/test.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXLENGTH 100
+#define MAXLINE 1000
+
+/*Print the longest input line*/
+
+/*read a line into S[],and output is its length */
+int getline(char line[], int maxline);
+/*copy content of from[] to to[];*/
+void copy(char to[], char from[]);
+
+
+int main()
+{
+	int len;
+	int max;
+	char line[MAXLINE];
+	char longest[MAXLINE];
+
+	max = 0;
+	while ((len = getline(line, MAXLINE)) > 0)
+	{
+		if (len > max)
+		{
+			max = len;
+			copy(longest, line);
+		}
+	}
+	if (max > 0)
+	{
+		printf("%s", longest);
+	}
+	return 0;
+}
+
+int getline(char line[], int maxline)
+{
+	int c, i;
+	for (i = 0; i < maxline - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		line[i] = c;
+		if (c == '\n')
+		{
+			line[i] = c;
+			++i;
+		}
+		line[i] = '\0';
+		return i;
+}
+void copy(char to[], char from[])
+{
+	int i;
+
+	i = 0;
+	while ((to[i] == from[i]) != '\0')
+	{
+		++i;
+	}
+}
